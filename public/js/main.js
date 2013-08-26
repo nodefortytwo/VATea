bd637be84bc592ab17cb22e683dcebc4b8436d8f
Rick Burgess <git@rickburgess.me>
2013-08-26 10:47:05 +0100

    Version 1 complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,31 @@
 (function($) {
 	$(document).ready(function(){
 		var id = 0;
-		$('#users input').bind('input', function(){
 
-			var node = $(this).parents('.input-row');
-			$(node).next().removeClass('hidden');
-
-			$('#winner').css('height', $('#users').height() + 'px');
-
-
-			//$('#winner h1').css('line-height', $('#winner').height() + 'px');
-
+		if($('.container').height() < $(window).height()){
+			$('.container').height($(window).height());
+		}
+		$('.height100').css('height', $('.container').height() + 'px');
+		$(window).resize(function() {
+			if($('.container').height() < $(window).height()){
+				$('.container').height($(window).height());
+			}
+	  		$('.height100').css('height', $('.container').height() + 'px');
 		});
 
-		$('#winner').bind('DOMNodeInserted DOMNodeRemoved', function(){
-			center();
-		})
+
+		$('.names input').bind('input', function(){
+
+			var node = $(this).parents('.input-row');
+			if($(node).next().hasClass('hidden')){
+				$(node).next().hide().removeClass('hidden').fadeIn();
+			}	
+		});
 
 		$('.go').click(function(){
 			var vals = [];
 
-			$('#users input').each(function(){
+			$('.names input').each(function(){
 
 				var row = $(this).parents('.input-row');
 
@@ -35,43 +40,34 @@
 			
 			if (vals.length == 0){
 				$('#reset').modal();
-			}
-
-
-			var i = 0;
-			var run = function(){
-				i++;
-
+			}else{
 				var randomIndex = Math.floor(Math.random() * vals.length);
 				var val = vals[randomIndex];
-				$('#name').html(val.name);
 
-				if(i < 10){
-					setTimeout(run, 100);
-				}else{
+				$('.state1').fadeOut(function(){
+					$('#name').html(val.name);
+					$('.state2').removeClass('hidden').hide().fadeIn();
 					$('[data-id="'+val.id+'"]').addClass('used');
-
-					var link = $('<a></a>').html('email them a reminder')
+					$('.share .scope-note').html('Let <strong>'+val.name+'</strong> know they lost')
+					$('.email').html('email them a reminder')
 							.attr('target', '_blank')
-							.attr('href', 'mailto:?subject=Make the Tea!&body=You were selected to make the tea!');
+							.attr('href', "mailto:?subject=Make the Tea!&body=Hey "+val.name+", You were picked to make the tea by Sage's VATea app! Get Brewing");
 
-					$('#link').html(link);
-				}
+					var tweet_text = 'Hey, ' + val.name + ' Make the tea!';
+					var tweet_url = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweet_text) + '&url=' + encodeURIComponent('http://www.sage.co.uk') + '&via=' + 'sageuk';
+					$('.twitter').attr('href', tweet_url);
+				});
 			}
-
-			setTimeout(run, 100);
-
-
-
+ 
 		});
 
-		$('#reset-btn').click(function(){
-			$('.used').removeClass('used');
-			$('#name').html('');
-			$('#reset').modal('hide');
-			$('#link').html('&nbsp;');
+		$('.replay').click(function(){
+			$('.state2').fadeOut(function(){
+					$('.state1').removeClass('hidden').hide().fadeIn();
+				});
 		})
 
+		$('#reset-btn').click(reset);
 
 	});
 
@@ -81,5 +77,13 @@
 		$('#winner .center').css('margin-left', '-' + ($('#winner .center').width() / 2) + 'px');
 		$('#winner .center').css('left', '50%');
 	}
+
+	var reset = function(){
+			$('.used').removeClass('used');
+			$('#name').html('');
+			$('#reset').modal('hide');
+			$('state1').show();
+			$('state2').hide();
+		}
 
 })(jQuery);
